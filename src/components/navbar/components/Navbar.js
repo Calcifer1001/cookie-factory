1ac5ef9bd70cf87f7d519908b624e20247b81a27
Calcifer1001 <dseidlertoken@gmail.com>
2021-10-06 11:12:19 -0300

    [MOD] navbar row view

---
+++ src/components/navbar/components/Navbar.js
@@ -0,0 +1,57 @@
+import NavbarItem from './NavbarItem';
+import ConnectWalletButton from './ConnectWalletButton';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import "../assets/navbar.css";
+
+import Home from "../../home/Home";
+import Minting from '../../minting/Minting';
+import Market from '../../market/Market';
+import FAQ from '../../faq/FAQ';
+
+const Navbar = (props) => {
+    return(
+        <div>
+            <div class="flex-row">
+                <img alt="logo.png"/>
+                <NavbarItem 
+                    displayText="Home"
+                    href="/"
+                />  
+                <NavbarItem 
+                    displayText="Minting"
+                    href="/minting"
+                />            
+                <NavbarItem 
+                    displayText="Market"
+                    href="/market"
+                />
+                <NavbarItem 
+                    displayText="FAQ"
+                    href="/faq"
+                />
+                <ConnectWalletButton
+                    displayText="Connect Wallet"
+                />
+            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                    <Route exact path="/minting">
+                        <Minting/>
+                    </Route>
+                    <Route exact path="/market">
+                        <Market/>
+                    </Route>
+                    <Route exact path="/faq">
+                        <FAQ/>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+
+export default Navbar;
