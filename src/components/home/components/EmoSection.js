803d42c8c7f06147d8eea3b7509ab0bdd43bb58f
Leandro Missoni <leomissoni@gmail.com>
2021-10-06 20:01:44 -0300

    [MOD] Modificaciones en la sección EmoSection

--- src/components/home/components/EmoSection.js
+++ src/components/home/components/EmoSection.js
@@ -1,13 +1,22 @@
 import "../assets/emotion.css";
 import EmoTitle from '../components/EmoTitle.js';
+import EmoText from "./EmoText";
 
 
 const EmoSection = (props) => {
     return(
-        <div>
-            <EmoTitle
-                displayText="Cookies are pretty fun!"
-            />  
+        <div class="row">
+            <div class="column">
+                <EmoTitle
+                    displayText="Cookies are pretty fun!"
+                />
+                <EmoText
+                    displayText="Bueno, vamos a ver que sale de esto fafñafñjafjfjfñlafjlaf añjfajffjlasf afsjñafjñafjñlsafjaf fdjañsfjfljasflñf afjñlafjlafjañsfjafff asñlfjfjjfñlf"
+                />
+            </div>
+            <div class="column">
+                <img alt="logo.png"/>
+            </div>
         </div>
     );
 }
