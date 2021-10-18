b827a1483fee517a8e25e88b6bfa656d502c8cbd
Leandro Missoni <leomissoni@gmail.com>
2021-10-18 18:20:16 -0300

    [ADD] style-components y prop-types agregados

--- src/components/faq/FAQ.tsx
+++ src/components/faq/FAQ.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import FaqComponent from './components/faqComponent';
 
 export default function faq() {
-    return <h1>This is faq</h1>;
-}
+    return (
+        <div>
+            <h1>This is faq</h1>
+            <FaqComponent />
+        </div>
+    );
+};
 
 
---
+++ src/components/faq/components/faqComponent.tsx
@@ -0,0 +1,9 @@
+const FaqComponent = (props: Record<string, any>) => {
+    return(
+        <div>
+            <h1> sdff </h1>
+        </div>
+    );
+};
+
+export default FaqComponent;
