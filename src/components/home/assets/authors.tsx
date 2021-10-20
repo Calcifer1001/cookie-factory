9d541a3abf5db59cd298bc37741f067fa304faae
Leandro Missoni <leomissoni@gmail.com>
2021-10-20 18:05:41 -0300

    [MOD] Seccion de autores con styled-components

---
+++ src/components/home/assets/authors.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const AuthorTitle = styled.div`
+        font-family: Verdana;
+        color: purple;
+        font-size: 300%;
+        background-color: lightskyblue;
+        text-align: center;
+        padding: 2%
+`
+
+export const AuthorName = styled.div`
+    font-family: Arial;
+    color: darkorchid;
+    font-size: 150%;
+    background-color: lightblue;
+    text-align: center;
+    padding : 5%;
+`
+
+export const AuthorText = styled.div`
+    font-family: Arial;
+    color: darkorchid;
+    font-size: 100%;
+    background-color: lightblue;
+    padding : 5%;
+`
+
+export const AuthorColumn = styled.div`
+    float: left;
+    width: 20%;
+`
