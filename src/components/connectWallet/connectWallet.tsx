4bfc2e2da3462550cb94aa4a7d6b729f0a1ec599
Leandro Missoni <leomissoni@gmail.com>
2021-11-08 16:38:56 -0300

    [ADD] Pagina para conectar Wallet creada (y agrago archivos)

---
+++ src/components/connectWallet/connectWallet.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import NearComponent from './components/nearComponent';
+// import NarWalletsComponent from './components/narwalletsComponent';
+
+export default function connectWallet() {
+    return (
+        <div>
+            <h1> Hola </h1>
+        </div>
+    );
+};
